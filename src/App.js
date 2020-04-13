32e37321330b58693ae9b45c3b4fab2801ac8f04
Claudio Busatto <cjcbusatto@gmail.com>
2020-04-13 14:17:33 +0200

    feat: include add repository

--- src/App.js
+++ src/App.js
@@ -11,7 +11,14 @@ function App() {
   }, []);
 
   async function handleAddRepository() {
-    api.post('repositories', {}).then(console.log);
+    const response = await api
+    .post('repositories', {
+      url: 'https://github.com/Rocketseat/umbriel',
+      title: `Umbriel ${Date.now()}`,
+      techs: ['Node', 'Express', 'TypeScript'],
+    });
+    
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
